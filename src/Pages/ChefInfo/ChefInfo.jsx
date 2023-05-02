900352ee594763e6893c8b302a23693b547d7e2d
Joy Purkistha <joypurokisthosh@gmail.com>
2023-05-02 15:24:49 +0600

    chef section

---
+++ src/Pages/ChefInfo/ChefInfo.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Button, Card, Container } from 'react-bootstrap';
+import './ChefInfo.css'
+
+
+const ChefInfo = ({ chef }) => {
+    console.log(chef.name)
+    const { _id, name, picture, years_of_experience, number_of_recipes, likes } = chef;
+    return (
+        <div>
+            <Card style={{ width: '18rem' }} className='h-100'>
+                <Card.Img variant="top" className='h-100' src={picture} />
+                <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text>
+                        Years of experience : {years_of_experience}
+                    </Card.Text>
+                    <Card.Text>
+                        Number of recipes : {number_of_recipes}
+                    </Card.Text>
+                    <Card.Text>
+                       Likes : {likes}
+                    </Card.Text>
+                </Card.Body>
+                <button className='btn btn-primary w-100 view-recipes-btn'>View recipes</button>
+            </Card>
+        </div>
+    );
+};
+
+export default ChefInfo;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import './Home.css'
+import { Container } from 'react-bootstrap';
+import ChefInfo from '../ChefInfo/ChefInfo';
 
 const Home = () => {
     const chefs = useLoaderData()
     return (
         <div className='mt-5'>
             <h1 className='text-center text-warning fw-bold fs-1 mt-5 mb-2'>Our Chefs</h1> <hr className='w-25 mx-auto mb-5 text-success' />
-            
+            <Container>
+                <div className='make-grid mt-5'>
+                    {
+                        chefs.map(chef => <ChefInfo
+                            key={chef._id}
+                            chef={chef}
+                        ></ChefInfo>)
+                    }
+                </div>
+            </Container>
         </div>
     );
 };
